7d1d9df6a38bc0d3070d1e7a6d9a4e3ebbabf171
Joaquin Bejar <jb@taunais.com>
2024-09-04 06:21:55 +0200

    Reorganize project structure and update crate description
    Add modules to split functionality into separate files, making the codebase more organized and maintainable. Update the project description in Cargo.toml to reflect the new purpose of the crate.

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,15 @@
     Email: jb@taunais.com 
     Date: 3/9/24
  ******************************************************************************/
- 
+
+pub mod config;
+
+pub mod application;
+
+pub mod presentation;
+
+pub mod session;
+
+pub mod transport;
+
+pub mod utils;
